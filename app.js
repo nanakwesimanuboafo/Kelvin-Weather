62c44f0740ee701e8eb0a29678722714df61abfb
nanakwesimanuboafo <93545721+nanakwesimanuboafo@users.noreply.github.com>
2021-11-29 18:21:30 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+// variable name kelvin stands for value 293
+const kelvin = 0;
+//variable name celsius stands for value of kelvin - 273 which is 293 - 273
+const celsius = kelvin - 273;
+//variable name fahrenheit stands for the value of celsius multiplied by the division of 9 by 5 in additionn to the integer 32;
+let fahrenheit = celsius * (9/5) + 32;
+// this code rounds down the value of fahrenheit
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+// Convert to Newton
+let newton = celsius * (33 / 100);
+ 
+// Round down
+newton = Math.floor(newton);
+ 
+console.log(`The temperature is ${newton} degrees Newton.`);
