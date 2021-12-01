957a358e71d8448fc690c717d20c29b912ba6c0e
nanakwesimanuboafo <93545721+nanakwesimanuboafo@users.noreply.github.com>
2021-12-01 19:55:22 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const kelvin = 0;
 //variable name celsius stands for value of kelvin - 273 which is 293 - 273
 const celsius = kelvin - 273;
-//variable name fahrenheit stands for the value of celsius multiplied by the division of 9 by 5 in additionn to the integer 32;
+//variable name fahrenheit stands for the value of celsius multiplied by the division of 9 by 5 in additionn to the number 32;
 let fahrenheit = celsius * (9/5) + 32;
 // this code rounds down the value of fahrenheit
 fahrenheit = Math.floor(fahrenheit);
